1a8a2b5fd1bb5427fa8e1136d5067d11ae27ce09
Asif Rasheed <b00073877@aus.edu>
2020-09-27 14:15:44 +0400

    Staging parser.cpp functions.h: Parser Implementation in C++
    Removed the existing implementation using Python. The implementation wasn't working on the test cases.
    Adding new files Parser.cpp and Functions.h for re implementing the parser in C++.
    
    Signed-off-by: Asif Rasheed <asif@linuxmail.org>

--- README.md
+++
@@ -1,2 +0,0 @@
-# Prolog-Parser
-We have built a recursive descent parser as part of our Programming Languages course project. 
---
+++ functions.h
@@ -0,0 +1,9 @@
+/**
+ Simple parser for PROLOG.
+ Derived from PROLOG parser project from the course Programming Languages (CMP321) at the American University of Sharjah.
+ */
+
+#define UPPER_CHAR 0
+#define LOWER_CHAR 1
+#define DIGIT 2
+#define UNKNOWN 99
